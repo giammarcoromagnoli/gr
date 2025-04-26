381caa35bb3961acdecad4b004d986fd651b28c2
giammarcoromagnoli <romagnoli.giammarco@gmail.com>
2025-04-26 21:02:53 +0200

    aggiunto componente product, costrutti if e for

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-product-list',
   imports: [],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.css'
+  styleUrl: './product-list.component.css',
 })
 export class ProductListComponent {
-
+  products: Array<Product> = [
+    { id: 1, title: 'product 1' },
+    { id: 2, title: 'product 2' },
+    { id: 3, title: 'product 3' },
+    { id: 4, title: 'product 4' },
+  ];
 }
